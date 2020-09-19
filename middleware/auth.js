4788930e0cbc2d7e40f01308cb40295494681688
Your Namejeorgedonato <you@example.comjeorgekhenr@gmail.com>
2020-09-19 11:27:56 -0700

    Modified package.json and middleware also added a couple of dependencies

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = async function(req,res,cb){
+    const token = req.header('x-auth-token');
+
+    //Check if token does not exists or created by session
+    if(!token) {
+        return res.status(401).json({msg: 'No token! Authorization denied'});
+    }
+
+    try {
+        //Verify the user's token
+        await jwt.verify(token,config.get('jwtSecretKey'),function(error,decoded){
+            if(error){
+                //If not verified
+                res.status(401).json({msg: 'Token is invalid!'});
+            }else{
+                //Include user in the request and continue with the route function
+                req.user = decoded.user;
+                cb();
+            }
+        });
+    } catch (err) {
+        console.log('Something wrong with the middleware');
+        res.status(500).json({msg: 'Server Error'});
+    }
+}
