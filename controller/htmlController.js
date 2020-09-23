e644e8bcb1732344da289c1ff6bec1146475c3cc
DuyenP <duyenpham97@gmail.com>
2020-09-22 19:02:31 -0700

    added codes to htmlController.js

--- controller/htmlController.js
+++ controller/htmlController.js
@@ -1,3 +1,34 @@
-module.exports = function (app) { 
+// html-routes.js - file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads frontpage.html
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/html/frontpage.html"));
+    });
+
+    // signup route loads signup.html
+    app.get("/signup", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/html/signup.html"));
+    });
+
+    // stocks route loads stocks.html
+    app.get("/stocks/view", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/html/stocks.html"));
+    });
+
+    // categories route loads categories.html
+    app.get("/categories/view", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/html/categories.html"));
+    });
 
 };
