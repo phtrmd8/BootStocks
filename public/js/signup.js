cf40c83031646bc2f9d889338445715a0149ab3b
jeorgedonato <jeorgekhenr@gmail.com>
2020-09-23 21:19:33 -0700

    Implementing monthly stock and modified js files

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
   //Appending JS Files
-  $.getScript("js/utils/isPrivate.js")
-    .done(function (script, textStatus) {
-      console.log(textStatus);
-    })
-    .fail(function (jqxhr, settings, exception) {
-      console.log("Triggered ajaxError handler.");
-    });
-  //End Here
+  (async function(){
+    await $.getScript("js/utils/isPublic.js");
+  })();
+
+  $('.signup').on('submit', function (e) {
+    e.preventDefault();
+    const firstname = $('#firstname').val().trim();
+    const lastname = $('#lastname').val().trim();
+    const username = $('#username').val().trim();
+    const email = $('#email-input').val().trim();
+    const password = $('#password-input').val().trim();
+    const cpassword = $('#cpassword-input').val().trim();
+
+    const userData = { firstname: firstname, lastname: lastname, username : username, email: email, password : password ,cpassword : cpassword };
+    createUser(userData);
+  });
+
+  function stateAlert(msg){
+    $('#alert').show();
+    $('#alert > .msg').html(msg);
+    setTimeout(() => { $("#alert").hide(); }, 1000);
+  }
 
   function createUser(userData) {
-    $.post('/users', userData)
+    $.post('/api/users', userData)
       .done(function (data) {
-        console.log(data)
+        // console.log(data)
         localStorage.setItem("token", data.token);
+        window.location.replace('/members');
       })
       .fail(function (err) {
-        console.log(err)
+        stateAlert(err.responseJSON);
       })
   };
 });
