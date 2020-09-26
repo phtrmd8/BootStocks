4fbdf7b1193bcb14dae706b6d8a165e1cd6e7e3f
jeorgedonato <jeorgekhenr@gmail.com>
2020-09-25 19:36:25 -0700

    Add Stock Form Html

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -1,31 +1,51 @@
 $(document).ready(function() {
 
-  let ticker = "aapl";
-  let apiKey = "32eb05433ffa0f75d6c5b3ba881d7b14f1c2a5f5"
-  let queryURL = `https://api.tiingo.com/tiingo/daily/${ticker}/prices?token=${apiKey}`;
+  // let ticker = "aapl";
+  // let apiKey = "32eb05433ffa0f75d6c5b3ba881d7b14f1c2a5f5"
+  // let queryURL = `https://api.tiingo.com/tiingo/daily/${ticker}/prices?token=${apiKey}`;
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (res) {
-    console.log(res);
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // }).then(function (res) {
+  //   console.log(res);
+  // });
+
+  // $(".enter-stock").on("submit", function(event) {
+    
+  //   event.preventDefault();
+
+  //   const newStock = {
+  //     ticker: $("#stock-input").val().toUpperCase().trim(),
+  //   };
+
+  //   $.ajax("/api/stocks", {
+  //     type: "POST",
+  //     data: newStock
+  //   }).then(function() {
+  //       console.log(`added ${newStock.ticker} to portfolio`);
+  //       location.reload();
+  //   });
+  // });
+
+
+  $("#search-ticker-btn").on('click',function(e){
+   const stockSymbol =  $('#stock-symbol').val().trim();
+   $.get(`https://cors-anywhere.herokuapp.com/https://api.tiingo.com/tiingo/utilities/search/${stockSymbol}?token=32eb05433ffa0f75d6c5b3ba881d7b14f1c2a5f5`)
+   .then(function(data){
+     let multiSelect = ''
+     data.forEach(d =>{ return multiSelect += `<option value='${d.ticker}'>${d.ticker} | ${d.name}</option>`});
+      // console.log(multiSelect)
+    //  $('.stockSelectDiv').show()
+    $('#multipleStockSelect').html(multiSelect);
+   }).catch(function(err){
+    console.log('Invalid Ticker');
+   });
   });
 
-  $(".enter-stock").on("submit", function(event) {
-    
-    event.preventDefault();
-
-    const newStock = {
-      ticker: $("#stock-input").val().toUpperCase().trim(),
-    };
-
-    $.ajax("/api/stocks", {
-      type: "POST",
-      data: newStock
-    }).then(function() {
-        console.log(`added ${newStock.ticker} to portfolio`);
-        location.reload();
-    });
+  $('#multipleStockSelect').on('change', function(e){
+    const symbol =  $(this).children("option:selected").val();
+    $('#stock-ticker').val(symbol)
   });
 
 });
